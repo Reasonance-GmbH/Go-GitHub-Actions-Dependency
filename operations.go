af394e42eab8159cc17da874772ac12bc1282179
Manuel Lang <manuellang183@gmail.com>
2021-03-07 17:02:01 +0100

    Create operations.go

---
+++ operations.go
@@ -0,0 +1,13 @@
+package Go-GitHub-Actions-Dependency
+
+var z int
+
+// Add adds to integers and returns the value
+func Add(x int, y int) int {
+	return x + y
+}
+
+// Sub subtracts to integers and returns the value
+func Sub(x int, y int) int {
+	return x - y
+}
